775ea86bdc1897f91c8bdfbe339582299264a074
Hoang Do <katievincy226@gmail.com>
2020-08-29 16:34:32 -0400

    Added cards

--- dist/effect.js
+++ dist/effect.js
@@ -2,3 +2,12 @@ function myFunction() {
     var popup = document.getElementById("yourPopup");
     popup.classList.toggle("show");
 }
+
+// function myFunction() {
+//     var x = document.getElementById("yourPopup");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
